050d541729a26aa2618ad85023f65bc752b82734
subbu <subbu.innovation@gmail.com>
2019-04-15 10:12:15 +0530

    Caching techniques

---
+++ Caching/Infinispan/SpringCachingInfinispan/src/main/java/com/javasampleapproach/infinispan/SpringCachingInfinispanApplication.java
@@ -0,0 +1,14 @@
+package com.javasampleapproach.infinispan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class SpringCachingInfinispanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCachingInfinispanApplication.class, args);
+	}
+}
